d35337d06e1b7311b3e45809f63d56c1b1dc6f92
MichaelZett <michael2.zoeller@gmail.com>
2021-10-23 13:32:28 +0200

    Calculator both ways

--- Calculator.java
+++ Calculator.java
@@ -10,21 +10,37 @@ public class Calculator {
 		// Lass 2 Zahlen nacheinander über die Konsole eingeben
 		// Gib deren Summe auf der Konsole aus
 		// Hinweise: Scanner, ggf. Konvertieren
+		// try with resources
 		try (Scanner sc = new Scanner(System.in)) {
-			System.out.println("Gib eine Zahl ein!");
-			int zahl1 = sc.nextInt();
-
-			System.out.println("Gib noch eine Zahl ein!");
-
-			int zahl2 = 0;
-			try {
-				zahl2 = sc.nextInt();
-			} catch (InputMismatchException e) {
-				System.out.println(sc.next() + " ist Keine Zahl, nochmal bitte!");
-				zahl2 = sc.nextInt();
-			}
-			System.out.println("Das Ergebnis lautet " + (zahl1 + zahl2));
+			doCalculating(sc);
 		}
+
+		// vor java 7
+		Scanner sc = null;
+		try {
+			sc = new Scanner(System.in);
+			doCalculating(sc);
+		} finally {
+			if (sc != null) {
+				sc.close();
+			}
+		}
+	}
+
+	private static void doCalculating(Scanner sc) {
+		System.out.println("Gib eine Zahl ein!");
+		int zahl1 = sc.nextInt();
+
+		System.out.println("Gib noch eine Zahl ein!");
+
+		int zahl2 = 0;
+		try {
+			zahl2 = sc.nextInt();
+		} catch (InputMismatchException e) {
+			System.out.println(sc.next() + " ist Keine Zahl, nochmal bitte!");
+			zahl2 = sc.nextInt();
+		}
+		System.out.println("Das Ergebnis lautet " + (zahl1 + zahl2));
 	}
 
 }
